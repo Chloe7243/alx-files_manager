4acc7f74fd650a5d3bc121a256b5f7efeaee1f07
Stephanie <Stephaniechloe21@gmail.com>
2024-05-14 11:10:28 +0100

    task 1

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.isConnected = false;
+    this.client = createClient();
+    this.client.on("connnect", (err) => (this.isConnected = true));
+    this.client.on("error", (err) => console.log(err));
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    const getValue = promisify(this.client.get).bind(this.client);
+    return getValue(key);
+  }
+
+  async set(key, value, duration) {
+    const setValue = promisify(this.client.set).bind(this.client);
+    setValue(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    const delValue = promisify(this.client.del).bind(this.client);
+    return delValue(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
