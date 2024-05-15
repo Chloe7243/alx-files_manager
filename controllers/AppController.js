4d3c6d7d2d59b316857275b6e384f20871c3a826
Stephanie <Stephaniechloe21@gmail.com>
2024-05-15 13:12:54 +0100

    Task 3

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,22 +2,14 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 exports.getStatus = (req, res) => {
-  const dbAlive = dbClient.isAlive();
-  const redisAlive = redisClient.isAlive();
-  if (redisAlive && dbAlive) {
-    res.send(JSON.stringify({ redis: redisAlive, db: dbAlive }));
-    res.statusCode = 200;
-  }
-  res.sendStatus(404);
+  const status = { redis: redisClient.isAlive(), db: dbClient.isAlive() };
+  res.status(200).send(status);
 };
+
 exports.getStats = async (req, res) => {
-  try {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
-    console.log(users);
-    res.send(JSON.stringify({ users, files }));
-    res.statusCode = 200;
-  } catch (err) {
-    res.sendStatus(404);
-  }
+  const stats = {
+    users: await dbClient.nbUsers(),
+    files: await dbClient.nbFiles(),
+  };
+  res.status(200).send(stats);
 };
