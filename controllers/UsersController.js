9158369d6da3ec05b17239b4f07c1b3bfd08e871
Stephanie <Stephaniechloe21@gmail.com>
2024-05-15 23:07:53 +0100

    task 4

---
+++ controllers/UsersController.js
@@ -0,0 +1,20 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+exports.postNew = (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    res.status(400).send('Missing email');
+  } else if (!password) {
+    res.status(400).send('Missing password');
+  } else if (dbClient.db.users.find({ email })) {
+    res.status(400).send('Already exist');
+  } else {
+    const hashedPassword = sha1(password);
+    const data = dbClient.db.products.insertOne({
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).send(data.insertedId);
+  }
+};
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import bodyParser from "body-parser";
 import mainRoutes from './routes/index';
+
 
 const PORT = process.env.DB_PORT || '5000';
 
 const app = express();
 
 app.use('/', mainRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
