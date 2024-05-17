4872aef687d2ca8f4b2ed1f473948fd067901a9f
Stephanie <Stephaniechloe21@gmail.com>
2024-05-17 10:45:49 +0100

    tasks

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,20 +2,27 @@ import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 export default class UsersController {
-  static postNew(req, res) {
+  static async postNew(req, res) {
+    let user;
     const { email, password } = req.body;
+    await dbClient.users.findOne({ email }).then((result) => {
+      user = result;
+    });
+    console.log({ user });
+
     if (!email) {
       res.status(400).send('Missing email');
     } else if (!password) {
       res.status(400).send('Missing password');
-    } else if (dbClient.db.users.find({ email })) {
+    } else if (user) {
       res.status(400).send('Already exist');
     } else {
       const hashedPassword = sha1(password);
-      const data = dbClient.db.products.insertOne({
+      const data = await dbClient.users.insertOne({
         email,
         password: hashedPassword,
       });
+      console.log(data);
       res.status(201).send(data.insertedId);
     }
   }
