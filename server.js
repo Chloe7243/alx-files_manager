369c6e67ee465912b549c81c81d095c6de96b5f1
Stephanie <Stephaniechloe21@gmail.com>
2024-05-18 22:58:38 +0100

    fixed?

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mainRoutes from './routes/index';
 
 const PORT = process.env.DB_PORT || '5000';
 
 const app = express();
 
-app.use(bodyParser.json({ extended: false }));
+app.use(express.json());
 app.use('/', mainRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
